9cfdc3a1487e0d3778c420b84ebe639b81c751fb
Mathculator <91293429+Mathculator@users.noreply.github.com>
2023-07-13 16:59:03 -0700

    take picture
    open phone camera after clicking button

--- app/src/main/java/com/smarthealthyrecipe/MainActivity.java
+++ app/src/main/java/com/smarthealthyrecipe/MainActivity.java
@@ -11,7 +11,17 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.smarthealthyrecipe.databinding.ActivityMainBinding;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -32,6 +42,33 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        final int CAMERA_REQUEST_CODE = 123;
+        ActivityResultLauncher<Intent> cameraLauncher;
+        ImageView imageView = findViewById(R.id.imageView);
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Photo was taken successfully
+                        Intent data = result.getData();
+                        // Process the photo data
+                        Bundle extras = data.getExtras();
+                        Bitmap photoBitmap = (Bitmap) extras.get("data");
+                        // Do something with the photo
+                        imageView.setImageBitmap(photoBitmap);
+                    }
+                }
+        );
+
+        Button cameraButton = findViewById(R.id.button3);
+        cameraButton.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                cameraLauncher.launch(cameraIntent);
+            }
+        });
+
     }
 
 }
