17763f9938fa68470f2e8e433466039fbd8f76e1
rajataggarwal1 <rajataggarwal2@gmail.com>
2023-07-13 21:53:37 -0700

    Move code from main to DashboardFragment

--- app/src/main/java/com/smarthealthyrecipe/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/smarthealthyrecipe/ui/dashboard/DashboardFragment.java
@@ -1,15 +1,24 @@
 package com.smarthealthyrecipe.ui.dashboard;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.smarthealthyrecipe.R;
 import com.smarthealthyrecipe.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
@@ -23,6 +32,49 @@ public class DashboardFragment extends Fragment {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Access the buttons directly from the binding
+        binding.uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle upload button click
+            }
+        });
+
+        binding.enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle enter button click
+            }
+        });
+
+        final int CAMERA_REQUEST_CODE = 123;
+        ActivityResultLauncher<Intent> cameraLauncher;
+        ImageView imageView = requireView().findViewById(R.id.imageView);
+        cameraLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Photo was taken successfully
+                        Intent data = result.getData();
+                        // Process the photo data
+                        Bundle extras = data.getExtras();
+                        Bitmap photoBitmap = (Bitmap) extras.get("data");
+                        // Do something with the photo
+                        imageView.setImageBitmap(photoBitmap);
+                    }
+                }
+        );
+
+        Button cameraButton =  requireView().findViewById(R.id.takePictureButton);
+        cameraButton.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (cameraIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+                cameraLauncher.launch(cameraIntent);
+            }
+        });
+
+
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
