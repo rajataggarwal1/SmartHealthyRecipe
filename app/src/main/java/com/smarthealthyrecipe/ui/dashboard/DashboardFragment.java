9eb09767055ebbd08a7ce9423e4c42fe6f70da88
rajataggarwal1 <rajataggarwal2@gmail.com>
2023-07-15 13:09:06 -0700

    Add queries to fix camera issue

--- app/src/main/java/com/smarthealthyrecipe/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/smarthealthyrecipe/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,11 @@
 package com.smarthealthyrecipe.ui.dashboard;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -14,12 +16,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -31,6 +34,8 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     private FragmentDashboardBinding binding;
     private ActivityResultLauncher<Intent> cameraLauncher;
 
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1001;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +43,8 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         cameraLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
-                    Log.d("DashboardFragment", "Before Photo captured successfully");
+                    Log.d("DashboardFragment", "in launcher");
                     if (result.getResultCode() == Activity.RESULT_OK) {
-                        Log.d("DashboardFragment", "Photo captured successfully");
                         // Photo was taken successfully
                         Intent data = result.getData();
                         // Process the photo data
@@ -69,16 +73,15 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
     public void onClick(View view) {
         if (view.getId() == R.id.takePictureButton) {
             Log.d("DashboardFragment", "takePictureButton clicked");
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (cameraIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
-                Log.d("DashboardFragment", "takePictureButton clicked -- resolveActivity");
-                cameraLauncher.launch(cameraIntent);
-                Log.d("DashboardFragment", "takePictureButton clicked -- after Camera Launcher");
+            if (hasCameraPermission()) {
+                Log.d("DashboardFragment", " has camera permission");
+                launchCamera();
+            } else {
+                Log.d("DashboardFragment", " Does not have camera permission");
+                requestCameraPermission();
             }
-        }
-        else if(view.getId() == R.id.enterButton){
+        } else if (view.getId() == R.id.enterButton) {
             // Dialog code goes here
-            Log.d("DashboardFragment", "enterButton");
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setTitle("Enter Text");
 
@@ -87,42 +90,52 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
             final EditText editText = editView.findViewById(R.id.editText);
             builder.setView(editView);
 
-            Log.d("DashboardFragment", "enterButton-2");
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     String enteredText = editText.getText().toString();
-                    Log.d("DashboardFragment", "enterButton  text" + enteredText);
                     Toast.makeText(getActivity(), "Entered Text: " + enteredText, Toast.LENGTH_SHORT).show();
                 }
             });
             builder.setNegativeButton("Cancel", null);
-            Log.d("DashboardFragment", "enterButton  after" );
+
             AlertDialog dialog = builder.create();
             dialog.show();
         }
-//        AlertDialog.Builder builder = new AlertDialog.Builder(DashboardFragment.this);
-//        builder.setTitle("Enter Text");
-//
-//        // Create an EditText view and set it as the dialog's content
-//        final EditText editText = new EditText(DashboardFragment.this);
-//        builder.setView(editText);
-//
-//        // Set positive and negative buttons
-//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                String enteredText = editText.getText().toString();
-//                // Do something with the entered text
-//                // For example, display it in a Toast message
-//                Toast.makeText(MainActivity.this, "Entered Text: " + enteredText, Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        builder.setNegativeButton("Cancel", null);
-//
-//        // Create and show the AlertDialog
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+    }
+
+    private void launchCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Log.d("cameraIntent", " cameraIntent " + cameraIntent);
+//        cameraLauncher.launch(cameraIntent);
+        if (cameraIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            cameraLauncher.launch(cameraIntent);
+        }
+        else{
+            Log.d("cameraIntent", " in else " );
+            Toast.makeText(requireContext(), "No camera app found on the device", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission is granted, proceed with camera access
+                launchCamera();
+            } else {
+                // Camera permission is denied, show a message or handle the denial case
+                Toast.makeText(requireContext(), "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -131,3 +144,4 @@ public class DashboardFragment extends Fragment implements View.OnClickListener 
         binding = null;
     }
 }
+
